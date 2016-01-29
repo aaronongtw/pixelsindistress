0fc074cd14aad44481711ba15a7af445ebe9fed5
Danial Bakhsheshi <danial.bakhsheshi@danial.bakhsheshi.ansarada.lcl>
2016-01-30 00:38:53 +1100

    react conversion baby steps

--- game.js
+++ game.js
@@ -1,20 +1,7 @@
 
-window.OurGame = window.OurGame || {};
 
-window.OurGame.gameScreen = function(time, scores) {
-  var peeps = [];
-  for (var i = 0; i < scores.length; i++) {
-    peeps.push(<li>{scores[i]}</li>);
-  }
-  return <div>
-      <h1>{time}</h1>
-      <ul>
-        {peeps}
-      </ul>
-    </div>;
-};
 
 ReactDOM.render(
-  window.OurGame.gameScreen(3, [12, 5, 8]),
+  window.OurGame.room(10,4),
   document.getElementById('maindiv')
 );
---
+++ js/components.js
@@ -0,0 +1,12 @@
+window.OurGame = window.OurGame || {};
+
+window.OurGame.room = function(lvl, number) {
+  var Rooms = [];
+  for (var i = 0; i < number; i++) {
+    Rooms.push(<div><p id={'p'+i} class='stresslevel'>{lvl}</p>
+      <button class='avatar' id={'p'+i+'c'}></button></div>);
+  }
+  return <div id="room">
+            {Rooms}
+          </div>;
+};
