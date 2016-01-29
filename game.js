3b2cc087ef0e4056a24009ddd7de9e273b228e98
Paul Korzhyk <paul.korzhyk@gmail.com>
2016-01-30 00:21:42 +1100

    Reactify Aaron

--- game.js
+++ game.js
@@ -1,7 +1,20 @@
+
+window.OurGame = window.OurGame || {};
+
+window.OurGame.gameScreen = function(time, scores) {
+  var peeps = [];
+  for (var i = 0; i < scores.length; i++) {
+    peeps.push(<li>{scores[i]}</li>);
+  }
+  return <div>
+      <h1>{time}</h1>
+      <ul>
+        {peeps}
+      </ul>
+    </div>;
+};
+
 ReactDOM.render(
-  <div>
-    <h1>Pixels in distress</h1>
-    <h2>3 3 3</h2>
-  </div>,
+  window.OurGame.gameScreen(3, [12, 5, 8]),
   document.getElementById('maindiv')
 );
