26da836faa69d2ba33485fe84616aee346579d20
Paul Korzhyk <paul.korzhyk@gmail.com>
2016-01-30 18:01:41 +1100

    paul does stuff

--- js/components.js
+++ js/components.js
@@ -1,4 +1,3 @@
-window.OurGame = window.OurGame || {};
 window.OurGame.room = function(time, people, dialog, pickPerson, report = ()=>[]) {
   var Rooms = [];
   for (var i = 0; i < people.length; i++) {
